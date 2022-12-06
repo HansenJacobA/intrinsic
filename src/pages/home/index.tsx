b86b1326cc0785af39d8d032036f4c6893a5cea1
jake lovestolearn <jakelovestolearn@jakes-MBP.lan>
2022-12-06 00:52:48 -0600

    reset app

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,18 @@
 import Template from "../../components/template";
 import Mood from "../../components/mood";
 import GoalCard from "../../components/goalCard";
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import Greeting from "../../components/greeting";
+import seedDown from "../../utilities/seedDown";
 
 export default function Home() {
   return (
     <Flex justify="center" align="center" direction="column" gap={5} mb={10}>
       <Template />
-      <Greeting />
+      {/* <Greeting />
       <Mood />
-      <GoalCard />
+      <GoalCard /> */}
+      <Button onClick={() => seedDown()}>delete</Button>
     </Flex>
   );
 }
