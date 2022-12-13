260c7d898f1a59e65870129c7c2096966f94be7c
jake lovestolearn <jakelovestolearn@jakes-MacBook-Pro.local>
2022-12-12 21:33:30 -0600

    completes seed down function

--- src/utilities/removeValueByKey/index.ts
+++ src/utilities/removeValueByKey/index.ts
@@ -1,13 +1,9 @@
-import {
-  MonthsAndDaysByNumberAndDayIds,
-  HistoricalData,
-  Day,
-} from "../../types";
+import { HistoricalData, Day } from "../../types";
 import getValueByKey from "../getValueByKey";
 
 export default function removeValueByKey(
   key: string
-): MonthsAndDaysByNumberAndDayIds | HistoricalData | Day | Error {
+): HistoricalData | Day | Error {
   const valueToRemove = getValueByKey(key);
   if (valueToRemove === null) {
     return new Error("Key does not exist in storage");
--- src/utilities/seedDown/index.ts
+++ src/utilities/seedDown/index.ts
@@ -1,25 +1,13 @@
-import { Day } from "../../types";
-import { getCurrentYear } from "../getCurrentYear";
 import getValueByKey from "../getValueByKey";
 import removeValueByKey from "../removeValueByKey";
 
 // TODO: Write a test to ensure this works
 export default function seedDown(): void {
-  const historicalData = getValueByKey("historicalData");
-  let startingYear = historicalData.startingYear;
-  const endingYear = getCurrentYear();
-
-  while (startingYear.toString() !== endingYear) {
-    const yearData = getValueByKey(startingYear);
-    Object.values(yearData).forEach((monthNumber: number) => {
-      Object.values(monthNumber).forEach((dayNumber: number) => {
-        Object.values(dayNumber).forEach((id: Day["id"]) => {
-          removeValueByKey(id);
-        });
-      });
-    });
-    removeValueByKey(startingYear);
-    startingYear += 1;
-  }
+  removeValueByKey("lastDateUsed");
+  const allDayIds = getValueByKey("allDayIds");
+  allDayIds.array.forEach((id) => {
+    removeValueByKey(id);
+  });
+  removeValueByKey("allDayIds");
   removeValueByKey("historicalData");
 }
